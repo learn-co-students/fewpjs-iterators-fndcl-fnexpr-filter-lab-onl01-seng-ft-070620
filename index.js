72a7380347dda79a3fe1eca7a36fd8af08ec04b4
wickbushong <wickbushong@gmail.com>
2020-10-17 14:40:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, term) {
+    return arr.filter(e => e.toUpperCase() === term.toUpperCase())
+}
+
+function fuzzyMatch(arr, term) {
+    return arr.filter(e => e.startsWith(term))
+}
+
+function matchName(arr, term) {
+    return arr.filter(obj => obj.name === term)
+}
