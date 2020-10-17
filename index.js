2d85353810d888d70f6bc819b772cc625f7d5d54
cTraymany <chindalatht@gmail.com>
2020-10-17 16:42:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+// pry = require('pryjs');
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => {
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => {
+        return driver.startsWith(query)
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        return driver['name'] === name
+    })
+}
+
+// findMatching(drivers, "Bobby")
